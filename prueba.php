c36a05406cf70ec8471a2f5816db69702ac6aa09
Jorozco95 <1995orozco@gmail.com>
2021-09-11 10:21:24 -0600

    show clientes

--- prueba.php
+++ prueba.php
@@ -3,7 +3,8 @@
 <?php include("./includes/header.php"); ?>
 <?php include("./includes/footer.php"); ?>
  
-   <!-- <fieldset>
+   <!-- ------------------------------------------------------------Ingreso clientes ---------------------------------------------
+       <fieldset>
         <legend>Ingresar cliente</legend>
         <input type="text" name="Cod_Cliente" placeholder="Ingresa codigo"><br> 
         <input type="text" name="Nombre"><br> 
@@ -11,7 +12,7 @@
         <input type="text" name="Nit"><br> 
         <input type="text" name="Direccion"><br> 
         <button type="submit" id="accion" value="btnRegistrar" name="btnRegistrar" target="formCliente" >Registrar</button> 
-       
+    
     </fieldset> -->
     <div class="container p-4">
         <div class="row">
@@ -36,5 +37,36 @@
                     </fom>
                 </div>
             </div>
+        </div>
+    </div>
+<!-- ------------------------------------------------------------------Get clientes-------------------------------------------------------- -->
+<div class='container'>   
+        <div class="row">
+            <table>
+
+                <thead>
+                    <tr></tr>
+                    <th>Codigo cliente</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Nit</th>
+                    <th>Direccion</th>
+             </thead>
+             
+              <?php 
+              $data ="SELECT Cod_Cliente, Nombre, Apellido, Nit, Direccion FROM clientes";
+              $result = mysqli_query($conection, $data);
+              if(mysqli_num_rows($result)>0){
+                  while($row = mysqli_fetch_assoc($result)){
+                   ?>         
+             <tr>
+                 <td><?php echo $row["Cod_Cliente"] ?> </td>
+                 <td><?php echo $row['Nombre']?></td>
+                 <td><?php echo $row['Apellido']?></td>
+                 <td><?php echo $row['Nit']?></td>
+                 <td><?php echo $row['Direccion']?></td>
+             </tr>
+             <?php }  }  ?>  
+            </table>
         </div>
     </div>
